5b0a1c2e809c85894b62487e562831773320c1d5
Roudi22 <roudi.alzkemi@gmail.com>
2022-12-26 00:39:46 +0100

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e)=> {
 
   // fetch data from the server
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://roudiai.onrender.com", {
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
